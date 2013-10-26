0e7eafe851c081e98b73465ce5e68202a6d81b90
yakushiji <yaoshisizhenghe@gmail.com>
2013-10-26 12:32:41 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestCm::Application.config.secret_token = '0ce948ca32ff06c5f751cffb5b3468acecf02dc67579900786dc0171bfda2574e8d53ca88ab0d0d8b9439b28bd43f8457c653625219abce05db1d397a85aedb1'
